97debf246d5fac5d13a739c8a169273c1df933ce
ShanAbdulkarim <shanmank0@example.com>
2026-05-09 18:45:11 -0400

    styler

--- src/App.jsx
+++ src/App.jsx
@@ -10,18 +10,18 @@ function App() {
     <>
       <h1> Shan Abdulkarim Projects </h1>
       <h2> This is a compilation of my public works</h2>
-      <div>
-        <div>
-          <img src = "/STARTRS.png"/>
-          <a href = "startrs.vercel.app"> Startrs </a>
+      <div className='row'>
+        <div className='pillars'>
+          <img className='imager'src = "/STARTRS.png"/>
+          <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
         </div>
         <div>
-          <img src = "fpl.png"/>
-          <a href = "fpl-bot-frontend.vercel.app"> Fpl Bot </a>
+          <img className='imager' src = "fpl.png"/>
+          <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
         </div>
         <div>
-          <img src = "github.png"/>
-          <a href = "https://github.com/ShanAbdulkarim"> My Github </a>
+          <img className='imager' src = "github.png"/>
+          <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
         </div>
       </div>
     </>
