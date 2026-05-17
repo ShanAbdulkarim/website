0b4476ab0361830815cf89a0387ff4750d3cea0c
ShanAbdulkarim <shanmank0@gmail.com>
2026-05-17 16:29:02 -0400

    border

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,38 @@ function App() {
     <>
       <h1> Shan Abdulkarim Projects </h1>
       <h2> This is a compilation of my public works</h2>
-      <div className='row'>
-        <div className='pillars'>
-          <a href = "https://startrs.vercel.app"><img className='imager'  src = "/STARTRS.png"/></a>
-          <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
+      <div className='pillars'>
+        <div className='row'>
+          <div className='pillars'>
+            <a href = "https://startrs.vercel.app"><img className='imager'  src = "/STARTRS.png"/></a>
+            <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
+          </div>
+          <div class = "divider"></div>
+          <div className='pillars'>
+            <a href = "https://fpl-bot-frontend.vercel.app"><img className='imager' src = "fpl.png"/></a>
+            <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
+          </div>
+          <div class = "divider"></div>
+          <div className='pillars'>
+            <a href = "https://github.com/ShanAbdulkarim"><img className='imager' src = "github.png"/></a>
+            <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
+          </div>
         </div>
-        <div class = "divider"></div>
-        <div className='pillars'>
-          <a href = "https://fpl-bot-frontend.vercel.app"><img className='imager' src = "fpl.png"/></a>
-          <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
-        </div>
-        <div class = "divider"></div>
-        <div className='pillars'>
-          <a href = "https://github.com/ShanAbdulkarim"><img className='imager' src = "github.png"/></a>
-          <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
+        <div className='row'>
+          <div className='pillars'>
+            <a href = "https://startrs.vercel.app"><img className='imager'  src = "/STARTRS.png"/></a>
+            <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
+          </div>
+          <div class = "divider"></div>
+          <div className='pillars'>
+            <a href = "https://fpl-bot-frontend.vercel.app"><img className='imager' src = "fpl.png"/></a>
+            <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
+          </div>
+          <div class = "divider"></div>
+          <div className='pillars'>
+            <a href = "https://github.com/ShanAbdulkarim"><img className='imager' src = "github.png"/></a>
+            <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
+          </div>
         </div>
       </div>
     </>
