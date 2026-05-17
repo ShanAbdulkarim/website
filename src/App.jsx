5b4f4f796ff9e21de940c6aec57845927af79a06
ShanAbdulkarim <shanmank0@gmail.com>
2026-05-17 14:30:04 -0400

    link

--- src/App.jsx
+++ src/App.jsx
@@ -12,15 +12,15 @@ function App() {
       <h2> This is a compilation of my public works</h2>
       <div className='row'>
         <div className='pillars'>
-          <img className='imager' href = "https://startrs.vercel.app" src = "/STARTRS.png"/>
+          <a href = "https://startrs.vercel.app"><img className='imager'  src = "/STARTRS.png"/></a>
           <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
         </div>
         <div className='pillars'>
-          <img className='imager' href = "https://fpl-bot-frontend.vercel.app" src = "fpl.png"/>
+          <a href = "https://startrs.vercel.app"><img className='imager' href = "https://fpl-bot-frontend.vercel.app" src = "fpl.png"/></a>
           <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
         </div>
         <div className='pillars'>
-          <img className='imager' href = "https://github.com/ShanAbdulkarim" src = "github.png"/>
+          <a href = "https://startrs.vercel.app"><img className='imager' href = "https://github.com/ShanAbdulkarim" src = "github.png"/></a>
           <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
         </div>
       </div>
