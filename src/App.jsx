588ccdd447256eccdb4dce1407368077ee1366ec
ShanAbdulkarim <shanmank0@gmail.com>
2026-05-18 07:12:04 -0400

    apipal

--- src/App.jsx
+++ src/App.jsx
@@ -8,52 +8,27 @@ function App() {
 
   return (
     <>
-      <h1> Shan Abdulkarim Projects </h1>
-      <h3> This is a compilation of my public works</h3>
-
       <div className='pillars'>
-        <h2> Javascript Projects</h2>
-        <div className='row'>
-          <div className='pillars'>
-            <a href = "https://startrs.vercel.app"><img className='imager'  src = "/STARTRS.png"/></a>
-            <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
-          </div>
-          <div class = "divider"></div>
-          <div className='pillars'>
-            <a href = "https://fpl-bot-frontend.vercel.app"><img className='imager' src = "fpl.png"/></a>
-            <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
-          </div>
-          <div class = "divider"></div>
-          <div className='pillars'>
-            <a href = "https://github.com/ShanAbdulkarim"><img className='imager' src = "github.png"/></a>
-            <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
-          </div>
-          <div class = "divider"></div>
-          <div className='pillars'>
-            <a href = "https://apipal.vercel.app"><img className='imager' src = "apipal.png"/></a>
-            <a className = "link" href = "https://apipal.vercel.app"> Api-Pal </a>
-          </div>
-        </div>
+        <h1> Shan Abdulkarim Projects </h1>
+        <h3> This is a compilation of my public works</h3>
 
-        <div className='divsider' />
+        <div className='pillars' id = "phone">
+            <div className='row'>
+              <a href = "https://startrs.vercel.app"><img className='iphone'  src = "/STARTRS.png"/></a>
+              <a className = "link" href = "https://startrs.vercel.app"> Startrs </a>
 
-        <div className='row'>
-          <div className='pillars'>
-            <a href = "https://startrs.vercel.app"><img className='imager'  src = "/STARTRS.png"/></a>
-            <a className = "link" href = "https://startrs.vercel.app">  </a>
-          </div>
-          <div class = "divider"></div>
-          <div className='pillars'>
-            <a href = "https://fpl-bot-frontend.vercel.app"><img className='imager' src = "fpl.png"/></a>
-            <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
-          </div>
-          <div class = "divider"></div>
-          <div className='pillars'>
-            <a href = "https://github.com/ShanAbdulkarim"><img className='imager' src = "github.png"/></a>
-            <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
-          </div>
+              <a href = "https://fpl-bot-frontend.vercel.app"><img className='iphone' src = "fpl.png"/></a>
+              <a className = "link" href = "https://fpl-bot-frontend.vercel.app"> Fpl Bot </a>
+
+              <a href = "https://github.com/ShanAbdulkarim"><img className='iphone' src = "github.png"/></a>
+              <a className = "link" href = "https://github.com/ShanAbdulkarim"> My Github </a>
+
+              <a href = "https://apipal.vercel.app"><img className='iphone' src = "apipal.png"/></a>
+              <a className = "link" href = "https://apipal.vercel.app"> Api-Pal </a>
+            </div>
         </div>
       </div>
+
     </>
   )
 }
